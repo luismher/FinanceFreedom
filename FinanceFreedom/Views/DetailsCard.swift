fac7b1ab35ba1a59e872251713ae877084261986
luismher <luismher01@gmail.com>
2024-05-13 18:39:39 -0400

    finishing details card

--- FinanceFreedom/Views/DetailsCard.swift
+++ FinanceFreedom/Views/DetailsCard.swift
@@ -8,7 +8,12 @@
 import SwiftUI
 
 struct DetailsCard: View {
-    @State private var name = ""
+    @State private var date = "04/26/25"
+    @State private var type = "Income"
+    @State private var category = "Food"
+    @State private var amount = "15.83"
+    @State private var notes = ""
+    
     var body: some View {
         NavigationView{
             ZStack{
@@ -18,11 +23,46 @@ struct DetailsCard: View {
                         .frame(maxWidth: .infinity, alignment: .top)
                         .padding(.bottom, 60)
                     VStack (alignment: .leading, spacing: 30) {
-                        TextField("", text: $name)
-                            .textFieldStyle(.roundedBorder)
+                        Button("Edit"){}
+                            .frame(maxWidth: .infinity, alignment: .trailing)
+                            .padding(.top, 150)
                             .padding()
+                        Text(type)
+                            .frame(maxWidth: .infinity, alignment: .center)
+                            .padding(.top, -35)
+                            
+                        HStack {
+                            Text("Date")
+                                .padding(.leading, 20)
+                            Text("Category")
+                                .padding(.leading, 50)
+                            Text("Amount")
+                                .padding(.leading, 50)
+                        }
+                            .padding()
+                            .padding(.top, -35)
+                        HStack {
+                            TextField("", text: $date)
+                                .padding(.leading, -10)
+                            TextField("", text: $category)
+                                .padding(.leading, 10 )
+                            TextField("", text: $amount)
+                                .padding(.leading, 26)
+                        }.padding(.leading)
+                            .padding()
+                            .padding(.top, -55)
+                        
+                        TextEditor(text: $notes)
+                            .textFieldStyle(.roundedBorder)
+                            .cornerRadius(8)
+                            .overlay(RoundedRectangle(cornerRadius: 8).stroke(Color.gray, lineWidth: 1))
+                            .background(Color.gray.opacity(0.1))
+                            .frame(minHeight: 160)
+                            .padding()
+                            .padding(.bottom, 150)
+                            .padding(.top, -20)
                     }
-                    .frame(width: 350, height: 400)
+                    .frame(width: 350, height: 420)
                     .background(Rectangle().fill(.white))
                     .cornerRadius(10)
                     .padding()
