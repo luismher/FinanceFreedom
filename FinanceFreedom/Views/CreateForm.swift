c3d1b9a6b033342e39528fe67292fec3b98156b2
luismher <luismher01@gmail.com>
2024-05-14 00:16:48 -0400

    Create Form, created and finished uiview

---
+++ FinanceFreedom/Views/CreateForm.swift
@@ -0,0 +1,18 @@
+//
+//  CreateForm.swift
+//  FinanceFreedom
+//
+//  Created by Luismi on 5/13/24.
+//
+
+import SwiftUI
+
+struct CreateForm: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CreateForm()
+}
