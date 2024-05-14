37c9572081abcb58f77f72e631597ae0daa6dd69
luismher <luismher01@gmail.com>
2024-05-14 00:17:40 -0400

    Form UiView

--- FinanceFreedom/Views/CreateForm.swift
+++ FinanceFreedom/Views/CreateForm.swift
@@ -8,8 +8,49 @@
 import SwiftUI
 
 struct CreateForm: View {
+    @State private var test = ""
+    @State private var date = Date()
+    @State private var notes = ""
+    @State private var types = ["Income", "Expense"]
+    @State private var category = ["Food", "Gas", "Grocery", "Trips", "Vacation", "Rent"]
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            Form {
+                Section(header: Text("New Entry")){
+                    
+                    DatePicker("Date", selection: $date, displayedComponents: .date)
+                    Picker("Type", selection: $test) {
+                        ForEach(0 ..< types.count) {
+                            Text(self.types[$0]).tag($0)
+                        }
+                    }
+                    Picker("Category", selection: $test) {
+                        ForEach(0 ..< category.count) {
+                            Text(self.category[$0]).tag($0)
+                        }
+                    }
+                    TextField("Type Amount", text: $test)
+                        .multilineTextAlignment(.trailing)
+                }
+                Section(header: Text("Notes")){
+                    TextEditor(text: $notes)
+                        .textFieldStyle(.roundedBorder)
+                        .cornerRadius(8)
+                        .overlay(RoundedRectangle(cornerRadius: 8).stroke(Color.gray, lineWidth: 1))
+                        .background(Color.gray.opacity(0.1))
+                        .frame(minHeight: 160)
+                        .padding()
+                }
+            }
+            .navigationTitle("Create")
+            .toolbar {
+                ToolbarItemGroup(placement: .navigationBarTrailing) {
+                    Button("Cancel"){}
+                    Button("Save"){}
+                }
+            }
+        }
     }
 }
 
