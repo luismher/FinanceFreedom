3a4a1855673f664fc13fdd53488c723d578a13c8
luismher <luismher01@gmail.com>
2024-05-30 00:06:50 -0400

    SignIn with Google implemented and apple signin uiview started

---
+++ FinanceFreedom/Authentication/GoogleSignInMethod.swift
@@ -0,0 +1,8 @@
+//
+//  GoogleSignInMethod.swift
+//  FinanceFreedom
+//
+//  Created by Luismi on 5/29/24.
+//
+
+import Foundation
---
+++ FinanceFreedom/Utilities.swift
@@ -0,0 +1,18 @@
+//
+//  Utilities.swift
+//  FinanceFreedom
+//
+//  Created by Luismi on 5/29/24.
+//
+
+import SwiftUI
+
+struct Utilities: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Utilities()
+}
